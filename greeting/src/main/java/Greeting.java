bd12c94d80faae9c8914c9e573cfb3edcc55603e
Joel Lim <103879u@gmail.com>
2021-10-04 15:33:08 +0800

    pass two tests

--- greeting/src/main/java/Greeting.java
+++ greeting/src/main/java/Greeting.java
@@ -5,40 +5,74 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Greeting {
-    public static String greet(String... names) {
+
+    static String greetFormat(String prefix, String delimiter, String lastJoin, String suffix, String... names) {
         names = Arrays.stream(names)
                 .filter(Objects::nonNull)
                 .toArray(String[]::new);
 
         String joinedNames;
 
-        if (names.length == 0) {
-            joinedNames = "my friend";
-        } else {
-            List<String> components = new ArrayList<>();
+        List<String> components = new ArrayList<>();
 
-            if (names.length > 1) {
-                String nMinusOneNames = Arrays.stream(names)
-                        .limit(names.length - 1)
-                        .collect(Collectors.joining(", "));
-                String lastName = names[names.length - 1];
-                components.add(nMinusOneNames);
-                components.add(lastName);
+        if (names.length > 1) {
+            String nMinusOneNames = Arrays.stream(names)
+                    .limit(names.length - 1)
+                    .collect(Collectors.joining(delimiter));
+            String lastName = names[names.length - 1];
+            components.add(nMinusOneNames);
+            components.add(lastName);
+        } else {
+            components.add(names[0]);
+        }
+
+        joinedNames = String.join(lastJoin, components);
+        return String.format("%s%s%s", prefix, joinedNames, suffix);
+    }
+
+    /**
+     * @param names are assumed to be lowercase
+     * @return
+     */
+    private static String greetSoft(String... names) {
+        return greetFormat("Hello, ", ", ", "and", ".", names);
+    }
+
+    /**
+     * @param names are assumed to be uppercase
+     * @return
+     */
+    private static String greetShout(String... names) {
+        return greetFormat("HELLO, ", ", ", "AND", "!", names);
+    }
+
+    public static String greet(String... names) {
+        List<String> lowercaseNames = new ArrayList<>();
+        List<String> uppercaseNames = new ArrayList<>();
+
+        for (String name : names) {
+            if (name == null) {
+                continue;
+            } else if (name.chars().allMatch(Character::isUpperCase)) {
+                uppercaseNames.add(name);
             } else {
-                components.add(names[0]);
+                lowercaseNames.add(name);
             }
-
-            joinedNames = String.join(" and ", components);
         }
 
-        boolean isUpperCase = joinedNames.chars().allMatch(
-                Character::isUpperCase
-        );
-
-        if (isUpperCase) {
-            return String.format("Hello, %s!", joinedNames).toUpperCase();
-        } else {
-            return String.format("Hello, %s.", joinedNames);
+        if (names.length == 0) {
+            return greetSoft(new String[]{"my friend"});
         }
+
+        List<String> components = new ArrayList<>();
+
+        if (!lowercaseNames.isEmpty()) {
+            components.add(greetSoft(lowercaseNames.toArray(String[]::new)));
+        }
+        if (!uppercaseNames.isEmpty()) {
+            components.add(greetShout(uppercaseNames.toArray(String[]::new)));
+        }
+
+        return components.stream().collect(Collectors.joining(" AND "));
     }
 }
