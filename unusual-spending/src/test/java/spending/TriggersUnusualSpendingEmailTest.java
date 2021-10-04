68bb171c406623b1ba5bf77965e4518d7aa14349
Joel Lim <103879u@gmail.com>
2021-10-04 17:58:02 +0800

    add failing test

---
+++ unusual-spending/src/test/java/spending/TriggersUnusualSpendingEmailTest.java
@@ -0,0 +1,13 @@
+package spending;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TriggersUnusualSpendingEmailTest {
+
+	@Test
+	public void FailingTest(long userId) {
+		Assert.equals(true, false);
+	}
+
+}
